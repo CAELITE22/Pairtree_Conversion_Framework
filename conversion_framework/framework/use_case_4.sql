c5b853077cbd24923484f186834ae055dbca168b
matavt <55859839+matavt@users.noreply.github.com>
2022-08-17 19:42:27 +0930

    corrected usecase misunderstanding

--- conversion_framework/framework/use_case_4.sql
+++ conversion_framework/framework/use_case_4.sql
@@ -1,9 +1,4 @@
--- This function sets the default conversion set.
--- The default conversion ID is always -1
--- The default conversion is a copy of the reference conversion set to prevent inadvertent modification of the default.
-
-
-create or replace function  converter.set_default_conversion_set(
+create or replace function  converter.set_user_conversion_set(
     in_user_id int,
     in_cs_id int
 )
@@ -13,27 +8,36 @@ language plpgsql
 as
 $$
 declare
-    cs_name text;
+    conversion_set_name text;
 
 BEGIN
+    conversion_set_name = (select name from converter.conversion_set where id = in_cs_id)::text;
     if (in_cs_id is null or in_user_id is null) then
         return concat('conversion set or user ID can''t be NULL');
     end if;
+    
     if (in_cs_id not in (select id from converter.conversion_set)) then
         return concat('conversion_set_id does not exist');
     end if;
-
-    cs_name = (select name from converter.conversion_set where id = in_cs_id) :: text;
-
-    delete from converter.conversion_set where id = -1;
-    insert into converter.conversion_set (id, name, owner_user_id, created, updated, created_by, updated_by, active)
-    select -1, concat(name,'*')::text, -1, created, now(), created_by, in_user_id, active
-    from converter.conversion_set
-    where id = in_cs_id;
-
-    if (select count(*) from converter.conversion_set where name = concat(cs_name,'*')::text) = 1 then
-        return concat('default conversion set is now ',cs_name);
+    
+    if (in_user_id not in (select user_id from converter.user_conversion_set)) then
+        insert into converter.user_conversion_set (user_id, conversion_set_id, created, updated, updated_by, created_by, active)
+            values (in_user_id, in_cs_id,now(),now(),in_user_id,in_user_id,active);
     end if;
-    return concat('There was an error setting default conversion set');
+
+    if (in_user_id in (select user_id from converter.user_conversion_set)) then
+        update converter.user_conversion_set
+        set
+            conversion_set_id = in_cs_id,
+            updated_by = in_user_id,
+            updated = now()
+        where user_id = in_user_id;
+    end if;
+
+    if (select count(*) from converter.user_conversion_set where user_id = in_user_id and conversion_set_id = in_cs_id) = 1 then
+        return concat('user conversion set is now set to ', conversion_set_name );
+    end if;
+    
+    return concat('There was an error setting the conversion set');
 end;
 $$
