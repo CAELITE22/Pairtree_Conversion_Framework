a3e27258b4dd2cae4c492514b80475c9786970b7
clinnygee <clintongillespie@outlook.com>
2022-08-25 18:00:03 +1000

    added some tests for second function

--- conversion_framework/tests/4. test_use_case_6.sql
+++ conversion_framework/tests/4. test_use_case_6.sql
@@ -24,24 +24,25 @@ create or replace function converter_tests.test_use_case_6(
     select converter.get_id_from_data_type(1, 'use_case_6_test_dt')::text
     union all
     select converter.add_data_category(1, 'use_case_6_test_dc', (converter.get_id_from_data_type(1, 'use_case_6_test_dt'))::INT)
---     union all
---     select results_eq('select converter.update_default_conversion_set_category_uom(1, ''use_case_6_test_dc'', ''use_case_6_test_uom'')', 'select ''Error! The specified uom does not exist''', 'will not accept data type that doesnt exist')
---     union all
---     select converter.add_uom(
---                 1,
---                 converter.get_id_from_data_type(1, 'use_case_6_test_dt')::int,
---                 'use_case_6_test_uom',
---                 'uc6',
---                 1,
---                 NULL,
---                 NULL,
---                 NULL,
---                 NULL,
---                 true
---                )
---     union all
---     select results_eq('select converter.update_default_conversion_set_category_uom(1, ''use_case_6_test_dc'', ''use_case_6_test_uom'')', 'select ''Conversion set was updated successfully''', 'functions works correctly')
---     union all
---     select count(*)::text from converter.conversion_set
+    union all
+    select results_eq('select converter.update_default_conversion_set_category_uom(1, ''use_case_6_test_dc'', ''use_case_6_test_uom'')', 'select ''Error! The specified uom does not exist''', 'will not accept data type that doesnt exist')
+    union all
+    select converter.add_uom(
+                1,
+                converter.get_id_from_data_type(1, 'use_case_6_test_dt')::int,
+                'use_case_6_test_uom',
+                'uc6',
+                1,
+                NULL,
+                NULL,
+                NULL,
+                NULL,
+                true
+               )
+    union all
+    select results_eq('select converter.update_default_conversion_set_category_uom(1, ''use_case_6_test_dc'', ''use_case_6_test_uom'')', 'select ''Conversion set was updated successfully''', 'functions works correctly')
+    union all
+
+    select results_eq('update_target_conversion_set_category(1, ''failed_cs_name'', ''failed_dc_name'', ''failed_uom_name'')', '','')
 
 $$ language sql;
